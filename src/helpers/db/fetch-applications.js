d0058dca640e8457d7e8ad5cf2b742abee8fb987
Hugh FD Jackson <hugh@hughfdjackson.com>
2024-03-27 12:36:13 +0000

    Remove sites / applicants concepts
    Now the applicant and site details are stored with the application
    itself.
    
    This may cause a bit of data inconsistency between dataverse and this,
    because dataverse will hold a single customer record, whereas we'll have
    one customer record per application.
    
    The dataverse version will have the latest first name and last name.
    
    :D Another aspect of the design I'm happy to gloss over for a quick
    prototype.

--- src/helpers/db/fetch-applications.js
+++ src/helpers/db/fetch-applications.js
@@ -1,15 +1,26 @@
 async function fetchApplications() {
-  return Promise.resolve([
+  return [
     {
       applicationId: 'MLA/2024/00001',
-      applicantId: '7a78cd07-fe40-4659-bbf2-619eee442b1d',
-      siteId: 'df5d4b3a-83cb-4134-aa24-14e6efc197c1',
       title: 'The Seaside Serenity project',
       background:
         'The Seaside Serenity project aims to create a captivating and educational experience by establishing a petting zoo specifically tailored for sea horses. Located within a picturesque coastal setting, this endeavour seeks to promote marine conservation awareness while offering visitors a unique opportunity to interact with these fascinating creatures.',
-      submittedOn: new Date(2009, 0, 1) // 01/01/2009
+      applicant: {
+        firstName: 'John',
+        lastName: 'Haddock',
+        address: '29 Acacia Road, Nuttytown',
+        email: 'john@seahorseypettingzoo.com'
+      },
+      site: {
+        coordinates:
+          `53.589459598859236, -2.168875088935083
+            53.64902403831065, -2.045030160112487
+            53.544392862944882, -2.01696876060703
+            53.539634198638801, -2.028943897084951
+            53.589459598859236, -2.16887508893508`
+      }
     }
-  ])
+  ]
 }
 
 export { fetchApplications }
